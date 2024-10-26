25e7644f794f911ad9424d9fb09a227bea9d1bba
deventw <deven.tw@outlook.com>
2024-10-26 20:53:34 +0800

    update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: "DevenTW's homepage",
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://www.deven.tw',
+  siteRepo: 'https://github.com/deventw/deventw-homepage',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo_deven.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   // mastodon: 'https://mastodon.social/@mastodonuser',
